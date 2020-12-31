1300bc5efc713e23ae8b948d75a2edcab8b9bb99
Safal <76725946+Network-safal@users.noreply.github.com>
2020-12-31 21:41:52 +0530

    Create madlibs.py

---
+++ Noob_coding/Day1/madlibs.py
@@ -0,0 +1,9 @@
+#/usr/bin/python
+
+color = input("enter a color : ")
+plural_noun = input("enter a plural noun : ")
+celeb = input("enter a celebritie's name : ")
+
+print("roses are " +color)
+print(plural_noun +" are blue")
+print("I love " +celeb)
