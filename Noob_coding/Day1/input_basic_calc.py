ef2442312578f2b67b7bc08c4f1bf97731cfaa65
Safal <76725946+Network-safal@users.noreply.github.com>
2020-12-31 18:04:13 +0530

    Create input_basic_calc.py

---
+++ Noob_coding/Day1/input_basic_calc.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+name = input("enter name : ")
+age = input("enter age : ")
+print("Hello " +name+ "!, Its great to meet a " +age+ " year old human." )
+
+num1 = float(input("Enter a number : "))
+num2 = float(input("Enter another number : "))
+ans = num1 + num2
+print("Your answer is : " + str(ans))           #use str() is necessary when we want to concatenate
