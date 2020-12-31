8c0033787421a1b5f2b6680c18ceb126b9a8f71c
Safal <76725946+Network-safal@users.noreply.github.com>
2020-12-31 17:54:17 +0530

    Update and rename var_and_datatypes to var_and_datatypes.py

---
+++ Noob_coding/Day1/var_and_datatypes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+character_name = "naresh"   #strings
+character_age = "35"        #strings
+print("there was once a man called "+character_name+",")    #concatenate
+print("he was "+character_age+" years old.")
+
+#datatypes
+character_name = "hakka"    #changes the variables value halfway through the program
+character_age = 45          #number
+is_male = True              #boolean
+print("he really liked his name " +character_name+",")
+print("but he liked his age number "+str(character_age)+" more :p.")    #concatenate a integer into a string
