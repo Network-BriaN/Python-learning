8bb7e948f285494468d869cd930fa17e2d155521
Safal <76725946+Network-safal@users.noreply.github.com>
2021-01-02 18:05:00 +0530

    Create while_loop.py

---
+++ Noob_coding/Day3/while_loop.py
@@ -0,0 +1,12 @@
+import time         #for delay
+i = 10
+
+while i >= 1:                       #if true, jump to next statement; else, escape the loop
+    print(str(i)+ " secs to go")
+    time.sleep(1)                   #setting delay for 1 sec after every countdown
+    i -= 1
+
+time.sleep(1)                       #setting delay for 1 sec after while loop
+print()
+print()
+print("BOOOOMMMM!!!!!!")
